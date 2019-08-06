da64fd509e686e7a5ef27ab5b9e46559c6d16ec3
ZeekoZhu <vaezt@outlook.com>
2019-08-06 22:35:08 +0800

    feat(default-template): add style for different type

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -8,13 +8,36 @@ import { NotificationData } from '../../../notify-lite/src/lib/notification-data
 })
 export class AppComponent {
     title = 'playground';
-    data: NotificationData = {
-        autoDismiss: false,
-        message: 'Amet soufflé carrot cake tootsie roll jelly-o chocolate cake.',
-        pauseOnHover: true,
-        autoDismissTimeout: 2000,
-        type: 'success'
-    };
+    data: NotificationData[] = [
+        {
+            autoDismiss: false,
+            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
+            pauseOnHover: true,
+            autoDismissTimeout: 2000,
+            type: 'success'
+        },
+        {
+            autoDismiss: false,
+            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
+            pauseOnHover: true,
+            autoDismissTimeout: 2000,
+            type: 'info'
+        },
+        {
+            autoDismiss: false,
+            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
+            pauseOnHover: true,
+            autoDismissTimeout: 2000,
+            type: 'warning'
+        },
+        {
+            autoDismiss: false,
+            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
+            pauseOnHover: true,
+            autoDismissTimeout: 2000,
+            type: 'error'
+        },
+    ];
 
     constructor() {
     }

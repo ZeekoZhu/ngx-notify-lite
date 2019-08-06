46ba43ff418681e369185f684a23e531c1a8e203
ZeekoZhu <vaezt@outlook.com>
2019-08-06 23:35:55 +0800

    feat(default-template): add indicator animation

--- projects/notify-lite/src/lib/default-notify-template/default-notify-template.component.ts
+++ projects/notify-lite/src/lib/default-notify-template/default-notify-template.component.ts
@@ -1,18 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { NotificationData } from "../notification-data";
+import { animate, AnimationBuilder, AnimationMetadata, AnimationPlayer, style } from "@angular/animations";
 
 @Component({
     selector: 'ngx-default-notify-template',
     templateUrl: './default-notify-template.component.html',
     styleUrls: ['./default-notify-template.component.less']
 })
-export class DefaultNotifyTemplateComponent implements OnInit {
+export class DefaultNotifyTemplateComponent implements AfterViewInit {
     @Input() data: NotificationData;
+    @ViewChild('bar', { static: false }) barRef: ElementRef<HTMLDivElement>;
+    private player: AnimationPlayer;
+    private started = false;
 
-    constructor() {
+    constructor(private animationBuilder: AnimationBuilder) {
     }
 
-    ngOnInit() {
+    private barAnimation(): AnimationMetadata[] {
+        return [
+            style({ height: "100%" }),
+            animate(this.data.autoDismissTimeout, style({ height: 0 }))
+        ];
     }
 
+
+    start() {
+        const animation = this.animationBuilder.build(this.barAnimation());
+        this.player = animation.create(this.barRef.nativeElement);
+        this.player.play();
+        this.player.onDone(() => {
+            this.started = false;
+        });
+        this.started = true;
+    }
+
+    pause() {
+        if(this.player && this.started) {
+            this.player.pause();
+        }
+    }
+
+    resume() {
+        if(this.player && this.started) {
+            this.player.play();
+        }
+    }
+
+    ngAfterViewInit() {
+    }
 }
--- projects/notify-lite/src/lib/notify-lite.module.ts
+++ projects/notify-lite/src/lib/notify-lite.module.ts
@@ -3,12 +3,13 @@ import { OverlayModule } from "@angular/cdk/overlay";
 import { PortalModule } from "@angular/cdk/portal";
 import { DefaultNotifyTemplateComponent } from './default-notify-template/default-notify-template.component';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 @NgModule({
     declarations: [DefaultNotifyTemplateComponent],
     entryComponents: [],
-    imports: [OverlayModule, PortalModule, CommonModule],
+    imports: [OverlayModule, PortalModule, CommonModule, BrowserAnimationsModule],
     exports: [
         DefaultNotifyTemplateComponent
     ]

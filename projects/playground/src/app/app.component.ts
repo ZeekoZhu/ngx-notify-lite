4b617ccc14ba6c59efb9a385a3f74daad8bada75
ZeekoZhu <vaezt@outlook.com>
2019-08-07 23:14:27 +0800

    feat(playground): add interactive playground

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -8,41 +8,19 @@ import { NotifyCenterService } from "../../../notify-lite/src/lib/notify-center.
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'playground';
-    data: NotificationData[] = [
+    data: NotificationData =
         {
             autoDismiss: false,
             message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
             pauseOnHover: true,
-            autoDismissTimeout: 2000,
+            autoDismissTimeout: 10000,
             type: 'success'
-        },
-        {
-            autoDismiss: false,
-            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
-            pauseOnHover: true,
-            autoDismissTimeout: 2000,
-            type: 'info'
-        },
-        {
-            autoDismiss: false,
-            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
-            pauseOnHover: true,
-            autoDismissTimeout: 2000,
-            type: 'warning'
-        },
-        {
-            autoDismiss: false,
-            message: '【云闪付】恭喜您在“云闪付消费领红包”活动中获得0.1元的红包奖励，立即前往“云闪付APP首页-热门应用-领奖中心”领取（中奖7天内领取有效）。【中国银联】',
-            pauseOnHover: true,
-            autoDismissTimeout: 2000,
-            type: 'error'
-        },
-    ];
+        };
 
     constructor(private notifyCenter: NotifyCenterService) {
-        for(const item of this.data) {
-            notifyCenter.show(item);
-        }
+    }
+
+    notify() {
+        this.notifyCenter.show(this.data);
     }
 }
--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NotifyLiteModule } from "../../../notify-lite/src/lib/notify-lite.module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -10,7 +11,8 @@ import { NotifyLiteModule } from "../../../notify-lite/src/lib/notify-lite.modul
     ],
     imports: [
         NotifyLiteModule.forRoot(),
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
